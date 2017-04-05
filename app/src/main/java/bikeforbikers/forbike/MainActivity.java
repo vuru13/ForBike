55b218ebc6adb9c476898fd9bbdf12ac8c1fec9f
vuru13 <cemh.itu@gmail.com>
2017-04-05 16:27:46 +0300

    Initial commit

---
+++ app/src/main/java/bikeforbikers/forbike/MainActivity.java
@@ -0,0 +1,13 @@
+package bikeforbikers.forbike;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
